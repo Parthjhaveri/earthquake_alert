5cf8254d8a167d1cb9fa76bb81960f31a77df3a6
Parthjhaveri <parthjhaveri209@gmail.com>
2019-03-10 17:29:06 -0400

    started d3 implementation

--- src/frontend/components/graph/line-chart.jsx
+++ src/frontend/components/graph/line-chart.jsx
@@ -19,11 +19,13 @@ class Linechart extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.draw_chart = this.draw_chart.bind(this);
+
 		this.state = {}
 	}
 
 	componentDidMount() {
-		setTimeout(function() {
+		setTimeout(() => {
 
 			// EARTHQUAKES FROM FETCH FROM FEED.JSX
 			var parsed_quakes = (store.getState()).quake_data[0];
@@ -33,15 +35,16 @@ class Linechart extends React.Component {
 				quake.properties.mag >= 3 && quake.properties.mag < 4
 			);
 
-			const mag_three_parse = () => {
+			// DYNAMIC FUNCTION TO PARSE QUAKES
+			const mag_parse = (quakemag) => {
 				// NEW OBJECT
 				var mag_three_obj = new Object();
 				const quake_arr = []; // STORE QUAKES OBJECTS
 				
-				for (var i = 0; i < mag_three.length; i++) {
+				for (var i = 0; i < quakemag.length; i++) {
 
 					// GET DATE AND TIME OF EACH MAG 3 EARTHQUAKE
-					const time_stamp  = new Date(mag_three[i].properties.time);
+					const time_stamp  = new Date(quakemag[i].properties.time);
 					const string_time = time_stamp.toString();
 					const mag_three_time  = new Date(string_time.split(' ').slice(1, 5)).toLocaleString();
 					
@@ -49,7 +52,7 @@ class Linechart extends React.Component {
 					const quake_three_time = mag_three_time.split(' ');
 
 					// MAGNITUDE
-					const quake_three_mag = mag_three[i].properties.mag;				
+					const quake_three_mag = quakemag[i].properties.mag;				
 
 					mag_three_obj = {
 						emag:  quake_three_mag,
@@ -59,10 +62,35 @@ class Linechart extends React.Component {
 					quake_arr.push(mag_three_obj);
 				} // END LOOP 
 				console.log(quake_arr);
-			}
-			mag_three_parse();
+			} // END FUNCTION
+			
+			// PARSED MAGNITUDE THREE INVOCATION
+			const parsed_mag_three = mag_parse(mag_three);		
+			// this.draw_chart(parsed_mag_three);
 
 		}, 1000);
+	}
+
+	draw_chart(data) {
+		// SVG CONSTRUCTOR
+		let svg_width = 600;
+		let svg_height = 500;
+		let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+		let width = svg_width - margin.left - margin.right;
+		let height = svg_height - margin.top - margin.bottom;
+
+		// SET SVG WIDTH, HEIGHT
+		var svg = d3.select('svg')
+			.attr('width', svg_width)
+			.attr('height', svg_height);
+
+		// CREATE GROUP, THEN APPEND
+		var g = svg.append('g')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+		
+		// CREATE SCALES
+		var x = d3.scaleTime().rangeRound([0, width]);
+		var y = d3.scaleTime().rangeRound([height, 0]);
 	}
 
 	render() {		
