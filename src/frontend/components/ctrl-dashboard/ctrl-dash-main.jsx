8242e4c7c976b69b509678c54f272a1d7fdd726a
Parthjhaveri <parthjhaveri209@gmail.com>
2019-04-12 22:11:35 -0400

    setup dashboard structure

--- src/frontend/components/ctrl-dashboard/ctrl-dash-main.jsx
+++ src/frontend/components/ctrl-dashboard/ctrl-dash-main.jsx
@@ -15,22 +15,38 @@ class ControlDashboardMain extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.change_mag = this.change_mag.bind(this);
+
 		this.state = {	
 		}
 	}
 
-	componentDidMount() {
+	// CHANGE MAGNITUDE BUTTON
+	change_mag() {	
+		setTimeout(() => {
+			var section_dash = document.getElementById('main-dash-root');
+			// GENERATE 9 BUTTONS
+			for (var i = 1; i < 10; i++) {
+				// CREATE THE BUTTONS
+				var mag_btn = document.createElement('BUTTON');
 
+				// SET THE ATTRIBUTES AND APPEND
+				mag_btn.setAttribute('className', 'mag-button');
+				mag_btn.setAttribute('data-mag', '' + i);
+				section_dash.appendChild(mag_btn);
+			}
+		}, 500);
 	}
 
 	render() {
 		return (
-			<div className='section-main-dash'>
+			<div className='section-main-dash' id='main-dash-root'>
 				<h3>Sort By Magnitude</h3>
 				<p>
 					Select a magnitude button to 
-					cycle through the graph below <i class="fas fa-angle-down"></i>
+					cycle through the graph below <i className="fas fa-angle-down"></i>
 				</p>
+				{this.change_mag()}
 			</div>
 		)
 	}
